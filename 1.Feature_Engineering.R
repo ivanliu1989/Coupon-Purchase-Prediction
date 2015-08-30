978b9888f8133de26cdf88f00dced4e826d3ed5b
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2015-08-30 11:29:53 +1000

    feature engineering

---
+++ 1.Feature_Engineering.R
@@ -0,0 +1,16 @@
+setwd('/Users/ivanliu/Downloads/kaggle/Coupon-Purchase-Prediction')
+rm(list=ls());gc()
+# list all data
+system("ls ../data/en/")
+system("head ../data/en/*.csv")
+# read in all the input data
+c_area_train <- read.csv("../data/en/coupon_area_test_en.csv")
+c_area_test <- read.csv("../data/en/coupon_area_train_en.csv")
+c_detail_train <- read.csv("../data/en/coupon_detail_train_en.csv")
+c_list_train <- read.csv("../data/en/coupon_list_train_en.csv")
+c_list_test <- read.csv("../data/en/coupon_list_test_en.csv")
+plocation <- read.csv("../data/en/prefecture_locations_en.csv")
+ulist <- read.csv("../data/en/user_list_en.csv")
+
+# purchase to valid period
+# purchase location / coupon location
