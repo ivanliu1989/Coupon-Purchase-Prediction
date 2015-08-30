a7d9bb6f2df6cab1a4a454929996abc2316c2820
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2015-08-30 23:08:43 +1000

    feature engineering

--- 1.Feature_Engineering.R
+++ 1.Feature_Engineering.R
@@ -68,7 +68,7 @@ save(all, file='../data/model_based_data.RData')
 ### 4.Geographic features
 rm(list=ls());gc()
 load('../data/model_based_data.RData')
-plocation <- read.csv("../data/en/prefecture_locations_en.csv")
+plocation <- read.csv("../data/en/prefecture_locations_en.csv",stringsAsFactor=F)
 # en_ken (shop prefecture) | en_pref (customer prefecture)
 library(geosphere)
 distance <- do.call(rbind, lapply(1:nrow(plocation), FUN=function(i){
@@ -78,12 +78,22 @@ distance <- do.call(rbind, lapply(1:nrow(plocation), FUN=function(i){
     }))
     return(dist)
 }))
+distance <- as.data.frame(distance)
 names(distance) <- c('en_pref','en_ken', 'distance_km')
-all_loc <- merge(all, distance, all.x)
+distance$distance_km <- as.numeric(levels(distance$distance_km))[distance$distance_km]
+all_loc <- merge(all, distance, all.x=T, by=c('en_pref','en_ken'))
+
+save(all_loc, file='../data/model_based_data.RData')
 
 ### 5.Dropout/New customers
 dropout <- ulist[which(as.POSIXlt(ulist$WITHDRAW_DATE, format = "%Y-%m-%d") <= as.POSIXlt("2012-06-30", format = "%Y-%m-%d")),]
 newregister <- ulist[which(as.POSIXlt(ulist$REG_DATE, format = "%Y-%m-%d") > as.POSIXlt("2012-06-24", format = "%Y-%m-%d")),]
 
-### 6.Save cleaned dataset
+### 6.Sort/Generate Features
+
+
+### 7.One-hot Encoding
+
+
+### 8.Output cleaned dataset (train, validation, test)
 save(train, test, dropout, newregister, file='model_based_data.RData')
